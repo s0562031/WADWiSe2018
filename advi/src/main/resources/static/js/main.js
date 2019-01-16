736bbca2374dc84325165439e983e05fe27b59f4
kasiaone <s0553332@htw-berlin.de>
2019-01-16 01:37:49 +0100

    gui mit db anbindung

---
+++ advi/src/main/resources/static/js/main.js
@@ -0,0 +1,445 @@
+$( document ).ready(function() {
+	
+	// first time handle pages
+	handlePages();	
+	
+	// get window height
+	var windowHeight = $( window ).height();
+	var headerHeight = $('.a').height();
+	
+	// set grid to window height
+	//$('.c').height(windowHeight);
+	//$('#map').height(windowHeight-headerHeight);
+		
+	// submit login page form
+	$('#loginForm').on('submit', function(e) {		
+		 
+		 e.preventDefault(); // do not submit form
+		 
+		 var id = $('#loginForm input#userid').val();
+		 var pass = $('#loginForm input#password').val();
+		 
+		 // ajax request to check user password will be here
+		 var jqxhr = $.get( "/login?id=" + id + "&password=" + pass, function(data) {
+			 
+			 $.cookie("page", 2); 		 
+			 handlePages();	
+			 
+			 var adm = "normalo";
+			 
+			 if(data.isAdmin){
+				 adm = "admin";
+				 $('#addBtn').show();
+				 //$('#updateBtn').show();
+				 $('#deleteBtn').show();
+		 	 }
+		 
+			 $('#helloMsg').text("Hallo " + data.firstname + " " + data.lastname + " - Eingeloggt als " + adm);			 
+			 
+		 })
+	     .fail(function() {
+			   alert( "Kein User mit diesen Daten vorhanden!" );
+			 })	 
+	});
+	
+	// click on persons navigation
+	$('ul#navi').on('click', 'li', function(e) {		
+		$.cookie("page", 2); 
+		
+		$(this).parent().children().removeClass('active');
+		$(this).addClass('active');
+		
+		handlePages();		
+	});
+	
+	// click on add button
+	$('#addBtn').on('click', function(e) {		
+		$.cookie("page", 3); 				
+		handlePages();		
+		
+		$('form #firstname').val("");
+		$('form #lastname').val("");
+		$('form #street').val("");
+		$('form #city').val("");
+		$('form #postcode').val("");
+	});
+	
+	// click on update button
+	/*$('#updateBtn').on('click', function(e) {		
+					
+		var id = $("ul#navi li.active").attr("id");
+		
+		if(id != undefined) {
+			
+			$.cookie("page", 4); 				
+			handlePages();		
+			
+			var user = getUser(id);
+			
+			$('form #firstname').val(user.firstname);
+			$('form #lastname').val(user.lastname);
+			$('form #street').val(user.street);
+			$('form #city').val(user.city);
+			$('form #postcode').val(user.postcode);
+			
+		} else {
+			
+			alert("kein user ausgewählt!");
+		}
+	});*/
+	
+	// click on delete button
+	$('#deleteBtn').on('click', function(e) {		
+		
+		var id = $("ul#navi li.active").attr("id");
+		var name = $("ul#navi li.active").text();
+		
+		if(id != undefined) {
+			
+			if (confirm("User " + name + " löschen?")) {
+				  deleteUser(id);
+			} 
+			
+		} else {
+			
+			alert("Kein user ausgewählt!");
+		}
+	});
+	
+	function deleteUser(id){
+		
+		 var jqxhr = $.get( "/deleteUser", function(data) {
+			 $("li#" + id).remove();
+		 })
+	     .fail(function() {
+			   alert( "User konnte nicht gelöscht werden!" );	   
+		 })	 
+	}
+	
+	// click on map button
+	$('#showMapBtn').on('click', function(e) {		
+		
+		$.cookie("page", 2); 
+		handlePages();
+		
+		if($('#noUsers').is(':visible')){
+			loadContacts();	
+		}
+	});
+	
+	function loadContacts(data) {
+		
+		var jqxhr = $.get( "/getAllContacts", function(data) {
+			 
+			$("#noUsers").hide();
+			
+			$.each(data, function(k, user) {
+				
+				getLatLong(user);
+								
+				/*if($('#OpenLayers_Map_5_OpenLayers_ViewPort').length != 0){
+					addNewMarker(user);
+				}*/
+			});			
+		 })
+	     .fail(function() {
+			   alert( "Keine Kontakte vorhanden!" );	   
+		 })	 
+	}
+	
+	
+	// click on logout button
+	$('#logoutBtn').on('click', function(e) {		
+		$.removeCookie("page"); 
+		handlePages();	
+	});
+	
+	// click on show button
+		
+	// submit add form
+	$('#addForm').on('submit', function(e) {		
+		 
+		e.preventDefault(); // do not submit form	
+		
+		var data = {}		
+		data['id'] = localStorage.length;
+		
+		$(this).children().each(function(i,k) {
+			
+			if($(this).prop("type") == 'text') {
+								
+				data[$(this).prop("name")] = $(this).val();
+			}
+			
+			if($(this).prop("type") == 'checkbox') {
+				
+				data[$(this).prop("name")] = $(this).is(':checked');
+			}
+			
+			if($(this).prop("type") == 'select-one') {
+				
+				data[$(this).prop("name")] = $(this).children('option:selected').text();
+			}						
+		});
+						
+		saveData(data);		
+	});
+	
+	function saveData(data) {
+		
+		 console.log(data);
+		
+		 var jqxhr = $.get( "/addContact", function(data) {
+			
+			 // Kontakt hinzufügen per post *****
+			 
+			 $.cookie("page", 2); 
+			 handlePages();
+				
+			 getLatLong(data);
+					
+		 })
+	     .fail(function() {
+			   alert( "Kontakt konnte nicht hinzugefügt werden!" );	   
+		 })	 
+	}
+	
+	
+	$('#addFormBtn').on('click', function(e) {
+		
+		$('addForm').submit();
+	});
+	
+	/*$('#updateFormBtn').on('click', function(e) {
+			
+		$('addForm').submit();
+	});*/
+	
+	$('#deleteFormBtn').on('click', function(e) {
+		
+		$('addForm').submit();
+	});
+		
+	function getLatLong(user) {
+		
+		var street = user.address;
+		var city = user.city;
+		var postcode = user.postcode;
+		var address = city + "+" + street;
+				
+		var url= "https://nominatim.openstreetmap.org/search?q=" + address + "&format=json&limit=1";
+		
+		$.get( url, function(data) {
+			
+			if(data.length){
+				
+				user.lat = data[0]['lat'];
+				user.lon = data[0]['lon'];
+								
+				addUsertoBar(user);			
+				addNewMarker(user);
+			
+			} else {
+				
+				alert("address not found");
+			}
+			
+		}).fail(function() {
+				
+			console.log("error");
+		});
+	}
+	
+	function addUsertoBar(user){
+		
+		$("#noUsers").hide();
+		
+		$("ul#navi")
+			.append("<li>")
+			.children("li")
+			.last()
+			.text(user.firstname)
+			.attr("id",user.id)
+			.bind("click", function() {
+				openPopup(user.lon, user.lat);
+			});		
+	}
+	
+	function addNewMarker(user) {
+		
+		var address = "<b>" + user.firstname + " " + 
+					user.lastname + "</b><br />" + 
+					user.street + "<br />" + 
+					user.postcode + " " + 
+					user.city + "<br />" + 
+					user.country;
+		
+		newMarker(user.lon, user.lat, address);
+	}
+	
+	function handlePages(){
+		
+		// set page to show cookie: page: 1 = login, 2 = map, 3 = add, 4 = show
+		switch ($.cookie('page')) { 
+			case '1': 
+				$('#loginBox').show();
+				$('#mapBox').hide();
+				break;
+			case '2': 
+				$('#loginBox').hide();
+				$('#mapBox').show();
+				$('#map').show();
+				$('#formBox').hide();
+				$('#deleteBox').hide();
+				
+				// draw map only once				
+				if($('#OpenLayers_Map_5_OpenLayers_ViewPort').length == 0){
+					drawmap();
+				}
+				break;
+			case '3': 
+				$('#loginBox').hide();
+				$('#mapBox').show();
+				$('#map').hide();
+				$('#formBox').show();
+				$('#addFormBtn').show();
+				$('#updateFormBtn').hide();
+				$('#deleteBox').hide();
+				break;	
+			/*case '4': 
+				$('#loginBox').hide();
+				$('#mapBox').show();
+				$('#map').hide();
+				$('#formBox').show();
+				$('#addFormBtn').hide();
+				$('#updateFormBtn').show();
+				$('#deleteBox').hide();*/
+				break;
+			default:
+				$.cookie('page', 1); 
+				$('#loginBox').show();
+				$('#mapBox').hide();
+		}
+		
+		//console.log($.cookie('page'));
+	}
+});
+
+// Cookie Plugin
+/*!
+ * jQuery Cookie Plugin v1.4.1
+ * https://github.com/carhartl/jquery-cookie
+ *
+ * Copyright 2013 Klaus Hartl
+ * Released under the MIT license
+ */
+(function (factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['jquery'], factory);
+	} else if (typeof exports === 'object') {
+		// CommonJS
+		factory(require('jquery'));
+	} else {
+		// Browser globals
+		factory(jQuery);
+	}
+}(function ($) {
+
+	var pluses = /\+/g;
+
+	function encode(s) {
+		return config.raw ? s : encodeURIComponent(s);
+	}
+
+	function decode(s) {
+		return config.raw ? s : decodeURIComponent(s);
+	}
+
+	function stringifyCookieValue(value) {
+		return encode(config.json ? JSON.stringify(value) : String(value));
+	}
+
+	function parseCookieValue(s) {
+		if (s.indexOf('"') === 0) {
+			// This is a quoted cookie as according to RFC2068, unescape...
+			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+		}
+
+		try {
+			// Replace server-side written pluses with spaces.
+			// If we can't decode the cookie, ignore it, it's unusable.
+			// If we can't parse the cookie, ignore it, it's unusable.
+			s = decodeURIComponent(s.replace(pluses, ' '));
+			return config.json ? JSON.parse(s) : s;
+		} catch(e) {}
+	}
+
+	function read(s, converter) {
+		var value = config.raw ? s : parseCookieValue(s);
+		return $.isFunction(converter) ? converter(value) : value;
+	}
+
+	var config = $.cookie = function (key, value, options) {
+
+		// Write
+
+		if (value !== undefined && !$.isFunction(value)) {
+			options = $.extend({}, config.defaults, options);
+
+			if (typeof options.expires === 'number') {
+				var days = options.expires, t = options.expires = new Date();
+				t.setTime(+t + days * 864e+5);
+			}
+
+			return (document.cookie = [
+				encode(key), '=', stringifyCookieValue(value),
+				options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+				options.path    ? '; path=' + options.path : '',
+				options.domain  ? '; domain=' + options.domain : '',
+				options.secure  ? '; secure' : ''
+			].join(''));
+		}
+
+		// Read
+
+		var result = key ? undefined : {};
+
+		// To prevent the for loop in the first place assign an empty array
+		// in case there are no cookies at all. Also prevents odd result when
+		// calling $.cookie().
+		var cookies = document.cookie ? document.cookie.split('; ') : [];
+
+		for (var i = 0, l = cookies.length; i < l; i++) {
+			var parts = cookies[i].split('=');
+			var name = decode(parts.shift());
+			var cookie = parts.join('=');
+
+			if (key && key === name) {
+				// If second argument (value) is a function it's a converter...
+				result = read(cookie, value);
+				break;
+			}
+
+			// Prevent storing a cookie that we couldn't decode.
+			if (!key && (cookie = read(cookie)) !== undefined) {
+				result[name] = cookie;
+			}
+		}
+
+		return result;
+	};
+
+	config.defaults = {};
+
+	$.removeCookie = function (key, options) {
+		if ($.cookie(key) === undefined) {
+			return false;
+		}
+
+		// Must not alter options, thus extending a fresh object...
+		$.cookie(key, '', $.extend({}, options, { expires: -1 }));
+		return !$.cookie(key);
+	};
+
+}));
---
+++ advi/src/main/resources/static/js/tom.js
@@ -0,0 +1,131 @@
+var map;
+var layer_mapnik;
+var layer_tah;
+var layer_markers;
+
+function drawmap() {
+
+	OpenLayers.Lang.setCode('de');
+   
+    // Position und Zoomstufe der Karte - Berlin
+    var lon = 13.404954;
+    var lat = 52.520008;
+    var zoom = 11;
+
+    map = new OpenLayers.Map('map', {
+        projection: new OpenLayers.Projection("EPSG:900913"),
+        displayProjection: new OpenLayers.Projection("EPSG:4326"),
+        controls: [
+            new OpenLayers.Control.Navigation(),
+            new OpenLayers.Control.LayerSwitcher(),
+            new OpenLayers.Control.PanZoomBar()],
+        maxExtent:
+            new OpenLayers.Bounds(-20037508.34,-20037508.34,
+                                   20037508.34, 20037508.34),
+        numZoomLevels: 18,
+        maxResolution: 156543,
+        units: 'meters'
+    });
+
+    layer_mapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
+    layer_markers = new OpenLayers.Layer.Markers("Address", { 
+    		projection: new OpenLayers.Projection("EPSG:4326"), 
+   	    	visibility: true, displayInLayerSwitcher: false });
+
+    map.addLayers([layer_mapnik, layer_markers]);
+    jumpTo(lon, lat, zoom);
+}
+
+function newMarker(lon, lat, popuptext) {
+	
+   var lonLat = new OpenLayers.LonLat(lon,lat)
+          .transform(
+            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+            map.getProjectionObject() // to Spherical Mercator Projection
+          );
+          
+    var zoom=10;
+    var markers = new OpenLayers.Layer.Markers( "Markers" );    
+    
+    // dynamisch
+    addMarker(layer_markers, Number(lon), Number(lat), popuptext);
+    map.setCenter (lonLat, zoom);
+}
+
+function jumpTo(lon, lat, zoom) {
+    var x = Lon2Merc(lon);
+    var y = Lat2Merc(lat);
+    map.setCenter(new OpenLayers.LonLat(x, y), zoom);
+    return false;
+}
+
+function Lon2Merc(lon) {
+    return 20037508.34 * lon / 180;
+}
+
+function Lat2Merc(lat) {
+    var PI = 3.14159265358979323846;
+    lat = Math.log(Math.tan( (90 + lat) * PI / 360)) / (PI / 180);
+    return 20037508.34 * lat / 180;
+}
+
+function addMarker(layer, lon, lat, popupContentHTML) {
+
+	var ll = new OpenLayers.LonLat(Lon2Merc(lon), Lat2Merc(lat));
+    var feature = new OpenLayers.Feature(layer, ll); 
+    feature.closeBox = true;
+    feature.popupClass = OpenLayers.Class(
+    		OpenLayers.Popup.FramedCloud, {minSize: new OpenLayers.Size(300, 180) } );
+    feature.data.popupContentHTML = popupContentHTML;
+    feature.data.overflow = "hidden";
+    
+    console.log(feature);
+    
+    var marker = new OpenLayers.Marker(ll);
+    marker.feature = feature;
+
+    var markerClick = function(evt) {
+        if (this.popup == null) {
+            this.popup = this.createPopup(this.closeBox);
+            map.addPopup(this.popup);
+            this.popup.show();
+        } else {
+            this.popup.toggle();
+        }
+        OpenLayers.Event.stop(evt);
+    };
+    marker.events.register("mousedown", feature, markerClick);
+   
+    layer.addMarker(marker);
+    map.addPopup(feature.createPopup(feature.closeBox));
+}
+
+function openPopup(lon, lat){
+	var popups = map.popups;
+		
+	for(i=0; i<popups.length; i++){
+		var plon = popups[i]['lonlat']['lon'];
+		var plat = popups[i]['lonlat']['lat'];
+		
+		console.log(plon, plat);
+	}
+}
+
+function getCycleTileURL(bounds) {
+   var res = this.map.getResolution();
+   var x = Math.round((bounds.left - this.maxExtent.left) / (res * this.tileSize.w));
+   var y = Math.round((this.maxExtent.top - bounds.top) / (res * this.tileSize.h));
+   var z = this.map.getZoom();
+   var limit = Math.pow(2, z);
+
+   if (y < 0 || y >= limit)
+   {
+     return null;
+   }
+   else
+   {
+     x = ((x % limit) + limit) % limit;
+
+     return this.url + z + "/" + x + "/" + y + "." + this.type;
+   }
+}
