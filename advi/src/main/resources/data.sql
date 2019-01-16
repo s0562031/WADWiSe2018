cc84d64dd398449852ec69f0c58ad202d9f1214e
s0562031 <s0562031@htw-berlin.de>
2019-01-16 16:25:57 +0100

    firstname und lastname waren bei standardeintraegen in DB vertauscht

--- advi/src/main/resources/data.sql
+++ advi/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (id, lastname, firstname, password, is_admin) VALUES(1, 'Mueller', 'Tom', 'abc123', true);
 INSERT INTO users (id, lastname, firstname, password, is_admin) VALUES(2, 'Schmidt', 'Gabi', 'abc123', false);
 
-INSERT INTO contacts (id, lastname, firstname, address, city, postcode, country, privat) VALUES (1, 'Dustin', 'Schweitzberger', 'Loeperplatz', 'Berlin', '10367', 'Germany', true);
-INSERT INTO contacts (id, lastname, firstname, address, city, postcode, country, privat) VALUES (2, 'Heinrich', 'Scheffel', 'Dunckerstrasse', 'Berlin', '10439', 'Germany', false);
+INSERT INTO contacts (id, lastname, firstname, address, city, postcode, country, privat) VALUES (1, 'Schweitzberger', 'Dustin', 'Loeperplatz', 'Berlin', '10367', 'Germany', true);
+INSERT INTO contacts (id, lastname, firstname, address, city, postcode, country, privat) VALUES (2, 'Scheffel', 'Heinrich', 'Dunckerstrasse', 'Berlin', '10439', 'Germany', false);
